dc0fa7022f6be73886319fbbda9a936f02387359
Ben Hudson <ben@hudsonsathome.com>
2021-01-25 23:04:09 -0500

    added tensorflow model

---
+++ forward.sh
@@ -0,0 +1,6 @@
+if [ "$#" -ne 1 ]; then
+    echo "specify a port."
+    return 1
+fi
+
+ssh -K -N -L $1:localhost:$1 bh511@dev-gpu-bh511.cl.cam.ac.uk
---
+++ sync.sh
@@ -0,0 +1,12 @@
+if [ "$#" -ne 2 ]; then
+    echo "usage: sync.sh <file or folder> <direction>"
+    return 1
+fi
+
+if [ "$2" = "up" ]; then
+    rsync -av -e 'ssh -K' --progress --exclude-from=.rsyncignore $1 bh511@dev-gpu-bh511.cl.cam.ac.uk:~/$1
+elif [ "$2" = "down" ]; then
+    rsync -av -e 'ssh -K' --progress --exclude-from=.rsyncignore bh511@dev-gpu-bh511.cl.cam.ac.uk:~/$1 $1
+else
+    echo "invalid direction"
+fi
